86c27747d3948f1fa20cee4df478ad5e7d3f2107
Grayson Wright <wright.grayson@gmail.com>
2015-03-29 01:44:27 -0700

    Remove unused pages and scripts
    - `options.html`
    - `options.js`
    - `background.html`
    - `background.js`
    
    Also removed unused message passing between the content page
    and the background page.

--- background.js
+++
@@ -1,4 +0,0 @@
-chrome.extension.onMessage.addListener(function (request, sender, callback) {
-    var response = {};
-    callback(response);
-});
--- favicon.js
+++ favicon.js
@@ -2,21 +2,19 @@ var prStatus = function(status) {
   return $(".branch-status .status-description .text-" + status).length;
 };
 
-chrome.extension.sendMessage({}, function(response) {
-  $("body").bind("DOMSubtreeModified", function() {
-    var link = document.querySelector("link[rel~='icon']");
+$("body").bind("DOMSubtreeModified", function() {
+  var link = document.querySelector("link[rel~='icon']");
 
-    var faviconUrl = "https://assets-cdn.github.com/favicon.ico";
+  var faviconUrl = "https://assets-cdn.github.com/favicon.ico";
 
-    if($(".tabnav-pr").length) {
-      var icon = "pr";
+  if($(".tabnav-pr").length) {
+    var icon = "pr";
 
-      if(prStatus("success")) { icon = "pr_pass"; }
-      if(prStatus("failure")) { icon = "pr_fail"; }
+    if(prStatus("success")) { icon = "pr_pass"; }
+    if(prStatus("failure")) { icon = "pr_fail"; }
 
-      faviconUrl = chrome.extension.getURL("icons/" + icon + ".ico");
-    }
+    faviconUrl = chrome.extension.getURL("icons/" + icon + ".ico");
+  }
 
-    link.href = faviconUrl;
-  });
+  link.href = faviconUrl;
 });
