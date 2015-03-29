a3333a506052e5e5ba89bc87d8abe301c547caf6
Grayson Wright <wright.grayson@gmail.com>
2015-03-28 23:54:24 -0700

    Change favicons based on the status of the PR
    - Red PR icon for PRs that are failing CI
    - Green PR icon for PRs that are passing CI
    - Black PR icon for PRs with unknown CI status
    - Reset favicon back to octocat when navigating away from a PR
    
    The favicon updates whenever the DOM changes, so it should update when
    a new CI status is posted to the page.

--- favicon.js
+++ favicon.js
@@ -1,41 +1,22 @@
-chrome.extension.sendMessage({color: localStorage['color'], icon_size: localStorage['icon_size']}, function(response) {
+var prStatus = function(status) {
+  return $(".branch-status .status-description .text-" + status).length;
+};
 
-    if (response.cache_clear == 1) {
-        localStorage.removeItem('favicon');
-        localStorage.removeItem('color');
-        localStorage.removeItem('icon_size');
-    }
+chrome.extension.sendMessage({}, function(response) {
+  $("body").bind("DOMSubtreeModified", function() {
     var link = document.querySelector("link[rel~='icon']");
-    if (!link) {
-      link = document.createElement("link");
-      link.setAttribute("rel", "shortcut icon");
-      document.head.appendChild(link);
+
+    var faviconUrl = "https://assets-cdn.github.com/favicon.ico";
+
+    if($(".tabnav-pr").length) {
+      var icon = "pr";
+
+      if(prStatus("success")) { icon = "pr_pass"; }
+      if(prStatus("failure")) { icon = "pr_fail"; }
+
+      faviconUrl = chrome.extension.getURL("icons/" + icon + ".ico");
     }
-    link.type = "image/x-icon";
 
-    var faviconUrl = link.href || window.location.origin + "/favicon.ico";
-    var img = document.createElement('img');
-    img.addEventListener("load", onImageLoaded);
-    img.src = faviconUrl;
-
-    function onImageLoaded() {
-        if (localStorage["favicon"]) {
-            link.href = localStorage["favicon"];
-        } else {
-            var canvas = document.createElement("canvas");
-            var size = parseInt(response.icon_size);
-            canvas.width = size;
-            canvas.height = size;
-            var context = canvas.getContext("2d");
-            context.drawImage(img, 0, 0);
-            context.globalCompositeOperation = "source-in";
-            context.fillStyle = response.color;
-            context.fillRect(0, 0, size, size);
-            context.fill();
-            link.href = canvas.toDataURL();
-            localStorage["favicon"] = canvas.toDataURL();
-            localStorage["color"] = response.color;
-            localStorage["icon_size"] = size;
-        }
-    };
+    link.href = faviconUrl;
+  });
 });
